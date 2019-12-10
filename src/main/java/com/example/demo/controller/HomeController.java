342e1f3e587bf3354d6e84716cb8a8bc8fd2c57b
xcjing <xcjing@yeah.net>
2019-12-10 20:30:59 +0800

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-   // @GetMapping("")
-    @RequestMapping(value = "/hello", method= RequestMethod.GET)
+    @GetMapping("/hello")
+    //@RequestMapping(value = "/hello", method= RequestMethod.GET)
     public String hello() {
         return "Hello xcj!!!!!!!!!!!";
     }
